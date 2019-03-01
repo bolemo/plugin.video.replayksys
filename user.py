dac4b363c27d42ef880b7f0aa7d3a4c83113aacd
Thibault Buathier <thibault.buathier@gmail.com>
2019-03-01 19:42:15 +0100

    use pin code oauth

--- user.py
+++ user.py
@@ -8,6 +8,7 @@ import os.path
 import xbmcgui
 import xbmc
 import time
+import uuid
 
 class KsysUser:
 	def __init__(self):
@@ -26,19 +27,50 @@ class KsysUser:
 			self.accessToken 	= jwt['accessToken']
 			self.refreshToken 	= jwt['refreshToken']
 			self.expireToken 	= jwt['expireAccessTokenDate']
+			self.pin 			= jwt['pin']
+			self.playerId 		= jwt['playerId']
 		else:
 			self.accessToken 	= ""
 			self.refreshToken 	= ""
 			self.expireToken 	= 0
+			self.pin 			= ""
+			self.playerId 	    = ""
 
 	def saveJwt(self):
 		file = open(xbmc.translatePath("special://userdata/addon_data/pvr.ksys/.jwt"), "w")
 		file.write(json.dumps({
 			"accessToken":    			self.accessToken,
 			"expireAccessTokenDate": 	self.expireToken,
-			"refreshToken":      		self.refreshToken
+			"refreshToken":      		self.refreshToken,
+			"pin":      				self.pin,
+			"playerId":      			self.playerId
 		}))
 		file.close()
+
+	def getPinCode(self):
+		self.playerId = "kodi_plugin_replayksys_" + uuid.uuid4()
+		req = requests.get("%sv1/player/%s/setup" % (self.KAUTH_URL, self.playerId))
+		return json.loads(req.text)['pin']
+
+	def getTokenByCode(self):
+		dialog = xbmcgui.Dialog()
+		ok = xbmcgui.Dialog().yesno( "K-Sys Replay", "Vous devez connecter votre compte K-Sys pour continuer.", "", "Voulez-vous le faire maintenant ?", "Non","Oui")
+		self.pin = ""
+		while ok:
+			if self.pin == "":
+				self.pin = self.getPinCode()
+			dialog.ok("K-Sys Replay", "Rendez-vous sur https://reg.ktv.zone pour enregister ce lecteur %s avec votre compte. ENSUITE cliquez sur OK !" % (self.pin))
+			req = requests.get(self.KAUTH_URL+"v1/player/%s/access_token" % (self.pin))
+			if req.status_code == 200:
+				jwt = json.loads(req.text)
+				self.accessToken 	= jwt['access_token']
+				self.refreshToken 	= jwt['refresh_token']
+				self.expireToken 	= time.time() + jwt['expires_in']
+				self.saveJwt()
+				break;
+			elif req.status_code == 419 :
+				ok = dialog.yesno("K-Sys Replay", "Erreur le compte n'a pas été validé sur le site reg.ktv.zone" "", "Voulez-vous re-essayer ?", "Non","Oui")
+		return self.accessToken
 
 	def getJWTByPassword(self):
 		dialog = xbmcgui.Dialog()
@@ -54,7 +86,6 @@ class KsysUser:
 			)
 			if req.status_code == 200:
 				jwt = json.loads(req.text)
-				xbmc.log("AZERTY : Response " + req.text, xbmc.LOGDEBUG)
 				self.accessToken 	= jwt['access_token']
 				self.refreshToken 	= jwt['refresh_token']
 				self.expireToken 	= time.time() + jwt['expires_in']
@@ -80,13 +111,12 @@ class KsysUser:
 		)
 		if req.status_code == 200:
 			jwt = json.loads(req.text)
-			xbmc.log("AZERTY : Response " + req.text, xbmc.LOGDEBUG)
 			self.accessToken 	= jwt['access_token']
 			self.refreshToken 	= jwt['refresh_token']
 			self.expireToken 	= time.time() + jwt['expires_in']
 			self.saveJwt()
 		else:
-			return self.getJWTByPassword()
+			return self.getTokenByCode()
 
 		return self.accessToken
 
@@ -96,7 +126,7 @@ class KsysUser:
 		elif self.accessToken == "" and self.refreshToken != "":
 			return self.getJWTByRefreshToken()
 		else:
-			return self.getJWTByPassword()
+			return self.getTokenByCode()
 
 	def getCredentials(self):
 		return json.dumps({
@@ -131,7 +161,6 @@ class KsysUser:
 	:type url: str
 	"""
 	def getTempM3UCatchup(self, url):
-		xbmc.log("AZERTY : " + url, xbmc.LOGDEBUG)
 		req = requests.get(
 			"%s" % (url),
 			headers={"Authorization": "Bearer " + self.getAccessToken()}
